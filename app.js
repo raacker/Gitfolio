e9735da2635c6b11420f081975223cc783a0bbc1
raacker <haven.cpp@gmail.com>
2016-12-11 19:37:12 +0900

    refactored

--- app.js
+++ app.js
@@ -3,32 +3,41 @@ var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
+var http        = require('http');
+var url         = require('url');
+var routes       = require('./routes');
 
 // [CONFIGURE APP TO USE bodyParser]
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//app.get('/', routes.index);
+//app.get('/search', routes.search);
+//app.get('/users', routes.user);
+//app.get('/skillset', routes.skillset)
 
 // [CONFIGURE SERVER PORT]
 var port = process.env.PORT || 8081;
 
 // [RUN SERVER]
 var server = app.listen(port, function(){
- console.log("Express server has started on port " + port)
+  console.log("Express server has started on port " + port)
 
-var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function() {
-  console.log("Connected to MongoDB Server");
-});
+  var db = mongoose.connection;
+  db.on('error', console.error);
+  db.once('open', function() {
+    console.log("Connected to MongoDB Server");
+  });
 
-mongoose.connect('mongodb://localhost:27017/Gitfolio');
+  mongoose.connect('mongodb://localhost:27017/Gitfolio');
 
-var UserProfile = require('./models/UserProfile');
-var Repository = require('./models/Repository');
-var SkillSet = require('./models/SkillSet');
+  var UserProfile = require('./models/UserProfile');
+  var Repository = require('./models/Repository');
+  var SkillSet = require('./models/SkillSet');
 
-// [CONFIGURE ROUTER]
-var repositoryRouter = require('./routes/repository')(app, Repository);
-var userRouter = require('./routes/user')(app, UserProfile);
-var skillSetRouter = require('./routes/skillset')(app, SkillSet);
+  // [CONFIGURE ROUTER]
+  var repositoryRouter = require('./routes/repository')(app, Repository);
+  var userRouter = require('./routes/user')(app, UserProfile);
+  var skillSetRouter = require('./routes/skillset')(app, SkillSet);
+
 });
