ba3b77518c3eed6b33f7fbc6baf5f01c97bf0d46
raacker <haven.cpp@gmail.com>
2016-12-12 17:41:53 +0900

    add auth

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var url       = require('url');
 var express   = require("express");
 var router    = express.Router();
 var session   = require('express-session');
+var Client    = require('node-rest-client').Client;
 var db        = mongoose.connection;
+var github    = require('octonode');
+
+var client    = new Client();
 
 router.get("/", function(req, res) {
   res.status(200);
@@ -26,6 +30,24 @@ router.get("/main", function(req, res) {
 router.get("/login", function(req, res) {
   res.render("login");
 })
+
+router.get("/callback", function (req, res) {
+  var session_code = request.env['rack.request.query_hash']['code']
+
+  var args = {
+    data: {client_id: CLIENT_ID, client_secret: CLIENT_SECRET,
+          code: session_code, accept: json},
+   headers: {"Content-Type": "application/json"}
+  };
+
+  var result = client.post('https://github.com/login/oauth/access_token', args,
+                           function( data, response) {
+                             console.log(data);
+                             console.log(response);
+                           });
+
+  var access_token = JSON.parse(result)['access_token']
+});
 
 router.post("/", function(req, res) {
   res.render();
